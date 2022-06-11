923364a689aae528d1739d125fa9e4d0cd959528
Darwin Salinas <salinash2000@gmail.com>
2022-06-10 19:13:52 -0600

    Mostrando datos del usuario

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-console.log('Hola mundo')
-const nombre: string = 'Darwin'
-nombre = 9
+console.log('Hola mundo');
+const nombre: string = 'Darwin';
+const apellido: string = 'Salinas';
+const telefono: string = "88888888";
+
+console.log(nombre);
+console.log(apellido);
+console.log(telefono);
+
+
