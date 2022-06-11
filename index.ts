8937f17a5198f81a9932c48979e37f56f237da8e
Darwin Salinas <salinash2000@gmail.com>
2022-06-10 20:40:16 -0600

    funciones flecha

--- index.ts
+++ index.ts
@@ -1,14 +1,30 @@
-console.log('Hola mundo');
-const nombre: string = 'Darwin';
-const apellido: string = 'Salinas';
-const telefono: string = "88888888";
+const persona: {
+    nombre: string,
+    apellido: string,
+    telefono: number,
+    nombreCompleto: () => string,
+    telefonoToString: () => string,
+    obtenerIniciales: () => void
+} = {
+    nombre: 'Darwin',
+    apellido: 'Salinas',
+    telefono: 999999999,
+    nombreCompleto: function () {
+        console.log(this.nombre);
+        return this.nombre + ' ' + this.apellido
+    },
+    telefonoToString: function () {
+        return this.telefono.toString()
+    },
+    obtenerIniciales: function () {
+        return this.nombre[0] + ' ' + this.apellido[0]
+    }
+}
 
-console.log(nombre);
-console.log(apellido);
-console.log(telefono);
+const nombreCompleto = persona.nombreCompleto()
 
-
-
-
-
+console.log(nombreCompleto);
+console.log(persona.telefono);
+console.log(persona.telefonoToString());
+console.log(persona.obtenerIniciales());
 
